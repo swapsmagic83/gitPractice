0eaad9fd42be02c7e74b3c626f08a361f1d13e74
Kruti Rathod <swapsmagic83@gmail.com>
2023-06-30 23:15:57 -0700

    adding h3

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const h1= document.querySelector('h1')
 const h2=document.querySelector('h2')
+const h3=document.querySelector('h3')
 
 function randomColor(){
     let r= Math.floor(Math.random() * 256)
@@ -10,4 +11,5 @@ function randomColor(){
 const interval = setInterval(function(){
     h1.style.color=randomColor()
     h2.style.color=randomColor()
+    h3.style.color=randomColor()
 },2000)
